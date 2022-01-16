c0edc8b6588162fd4f0b5beecb7be72a0f293bdb
AzPerez <alejandrogibranperez@gmail.com>
2022-01-16 15:42:27 -0600

    React - App now imports MessageList and MessageForm components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
-import {
-  BrowserRouter,
-  Routes,
-  // Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import MessageList from "./components/MessageList";
-// import MessageForm from "./components/MessageForm";
+import MessageList from "./components/MessageList";
+import MessageForm from "./components/MessageForm";
 import Navigation from "./components/Navbar";
 
 import "bootswatch/dist/quartz/bootstrap.min.css";
@@ -16,8 +12,8 @@ function App() {
       <Navigation />
       <div className="container p-4">
         <Routes>
-          {/* <Route path="/" element={<MessageList />} /> */}
-          {/* <Route path="/new-message" element={<MessageForm />} /> */}
+          <Route path="/" element={<MessageList />} />
+          <Route path="/new-message" element={<MessageForm />} />
         </Routes>
       </div>
     </BrowserRouter>
